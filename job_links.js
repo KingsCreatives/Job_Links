f325aba698b1ce610565680b882a9f6b8f70961b
kwamecody <kwamecody@gmail.com>
2022-10-31 19:38:05 +0000

    completed small screen media query

--- job_links.js
+++ job_links.js
@@ -7,3 +7,18 @@ function toggleMenu() {
 }
 
 profileImg.addEventListener("click", toggleMenu);
+
+const sidebar = document.querySelector(".user-activities");
+const showMoreBtn = document.querySelector(".show-more-text");
+console.log(showMoreBtn);
+
+function showSideBar() {
+    sidebar.classList.toggle("open");
+    if (sidebar.classList.contains("open")) {
+        showMoreBtn.textContent = "Show less - ";
+    } else {
+        showMoreBtn.textContent = "Show more + ";
+    }
+}
+
+showMoreBtn.addEventListener("click", showSideBar);
